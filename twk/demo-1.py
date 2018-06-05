a4491739e221e3d2114249498fe0f650495b5689
tangweikun <819573105@qq.com>
2018-06-05 22:53:16 +0800

    :construction: Update demo-1

--- twk/demo-1.py
+++ twk/demo-1.py
@@ -2,3 +2,31 @@ if True:
     print('True')
 else:
     print("False")
+
+print("1" + "4")
+
+print("repeat-" * 3)
+
+str = 'try catch'
+
+print(str)  # try catch
+
+print(str[0:-1])  # try catc
+
+print(str[0:1])  # t
+
+print(str[0])  # t
+
+print(str[2:5])  # y c
+
+print(str[2:])  # y catch
+
+print(str * 2)  # try catchtry catch
+
+print(str + '你好')  # try catch你好
+
+# hello
+# world
+print('hello\nworld')
+
+print(r'hello\nworld')  # hello\nworld
