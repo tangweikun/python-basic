2aa9fe7c0c0cf99f29555977ef4fdf5cf0de3b04
tangweikun <819573105@qq.com>
2018-06-06 23:04:01 +0800

    :sparkles: isinstance && type

---
+++ twk/demo-05.py
@@ -0,0 +1,16 @@
+# type()不会认为子类是一种父类类型
+# isinstance()会认为子类是一种父类类型
+
+
+class A:
+    pass
+
+
+class B(A):
+    pass
+
+
+print(isinstance(A(), A))  # True
+print(type(A()) == A)  # True
+print(isinstance(B(), A))  # True
+print(type(B()) == A)  # False
