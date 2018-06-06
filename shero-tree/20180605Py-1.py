2c03fff786609f4c59d05008e26f5044e5e2fe08
sherotree <wytreeshero@163.com>
2018-06-06 22:37:12 +0800

    Update 20180605

--- shero-tree/20180605Py-1.py
+++ shero-tree/20180605Py-1.py
@@ -11,3 +11,29 @@ else:
 	print('ai')
 
 print('it'+'ou'+'py')
+
+print('op'+
+			'9'+
+			'ei')
+
+print(r'MR Tang is a fool\n heng')
+
+str='plausible'
+print(str)
+print(str[0:-1])
+print(str[0])
+print(str[2:5])
+print(str[3:])
+print(str*3)
+print(str+'hello')
+
+
+
+# x='q'
+# y='p'
+# print(x)
+# print(y)
+print("x", end='--')
+# print("x",end="--")
+# print(y,end=" ")
+# print()
