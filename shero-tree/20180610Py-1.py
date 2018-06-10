84166e326d1df920523987bc6efa0a620659a2ba
sherotree <wytreeshero@163.com>
2018-06-10 23:35:00 +0800

    Add  several

--- shero-tree/20180610Py-1.py
+++ shero-tree/20180610Py-1.py
@@ -1,8 +1,21 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 
 # Fibonacci series: 斐波纳契数列
 # 两个元素的总和确定了下一个数
 a, b = 0, 1
-while b < 10:
-    print(b)
+while b < 10000:
+    print(b, end='\n')
     a, b = b, a+b
+
+age = int(input("input your age："))
+print('')
+if age < 0:
+    print('are you kidding?')
+elif a < 18:
+    print('you are a cool teenager!')
+elif a < 38:
+    print('you are a cool younger!')
+elif a < 60:
+    print('you are a busy man!')
+else:
+    print('have a good body!')
