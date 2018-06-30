05c61724d4c4f0271f458a55b12791fbd78cbeea
sherotree <wytreeshero@163.com>
2018-06-30 19:00:38 +0800

    Add new

---
+++ shero-tree/20180612Py-1.py
@@ -0,0 +1,16 @@
+def area(width, height):
+    return width * height
+
+
+# def lo(name):
+#     print('hello', name)
+
+
+# lo('oh')
+# w = 5
+# h = 6
+# print('width=', w, 'height=', h, 'area=', area(w, h))
+
+vec = [2, 4, 6]
+m = [3 * x for x in vec]
+print(m)
