057978f74edab25e022f810fa9c30e26c466e91e
sherotree <wytreeshero@163.com>
2018-06-30 19:19:03 +0800

    ADD new

---
+++ shero-tree/20180630.py
@@ -0,0 +1,22 @@
+r = 25
+area = 3.114159*r*r
+print(area)
+print("{:.2f}F".format(area))
+
+
+import turtle
+turtle.pensize(2)
+turtle.circle(10)
+turtle.circle(40)
+turtle.circle(80)
+turtle.circle(100)
+
+
+from turtle import *
+color("red", "red")
+begin_fill()
+for i in range(5):
+    fd(200)
+    rt(144)
+end-fill()
+done()
