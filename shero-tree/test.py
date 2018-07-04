ee6d0ab8cb2ff412bbd55c932a40bbec8bae33b5
sherotree <wytreeshero@163.com>
2018-07-04 20:18:11 +0800

    New

--- shero-tree/test.py
+++ shero-tree/test.py
@@ -1,1 +1,12 @@
-print('H\ne\nl\nl\no\n\nW\no\nr\nl\nd')
+import turtle
+turtle.setup(500, 500, 100, 100)
+turtle.penup()
+turtle.fd(50)
+turtle.left(50)
+
+turtle.goto(100, 100)
+turtle.pendown()
+turtle.goto(100, -100)
+turtle.goto(-100, -100)
+turtle.goto(-100, 100)
+turtle.goto(100, 100)
